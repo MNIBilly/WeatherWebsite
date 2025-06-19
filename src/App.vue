e26c7e6d05ab0f8f3e505f158af5f0901e62d4a9
MNIBilly <billybktm07@gmail.com>
2025-06-19 10:17:36 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+
+function onInput(e) {
+  text.value = e.target.value
+}
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+
+  <input :value="text" @input="onInput" placeholder="Type here">
+  <p>{{ text }}</p>
+</template>
