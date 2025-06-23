88522bf2a20921e453d8090e74d1dd9abb904694
MNIBilly <billybktm07@gmail.com>
2025-06-23 15:15:37 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import "./styles.css"
 import SlotComp from './components/SlotComp.vue'
 
 const app = createApp(App)
 app.component('slot-comp', SlotComp)
 app.mount('#app')
+                  
