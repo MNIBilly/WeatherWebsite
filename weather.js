1f2ab6ad83882adf1dcac6cb5a12bd567b21da98
MNIBilly <billybktm07@gmail.com>
2025-07-01 15:20:56 +0200

    Update weather.js
    Added the 3-Day Forecast (just the days).
    Need to find a solution for the max and min temperature to visualize correctly.
    
    Added a graph for the temperature trend of the day, from midnight to 11pm.

--- weather.js
+++ weather.js
@@ -1,29 +1,45 @@
+// Chiave API per WeatherAPI
+
 const apiKey = 'ef1341cdcda24d17bc270728252406';
 
+// Giorno della settimana
 const dateObj = new Date();
 
 const getDayName = (dayType, dateVal = dateObj) => dateVal.toLocaleDateString('en-US', { weekday: dayType });
 
-
-/* Fetch weather data dell'API */
+// Fetch weather data dell'API */
 function fetchWeatherData(loc) {
     const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${loc}&aqi=no`;
 
+    // Controlla se l'API è raggiungibile
     fetch(apiUrl)
     .then(response => response.json())
+    // Controlla se la risposta è valida
     .then(data => {
         console.log(data);
         // Aggiorna i dati correnti
         document.querySelector('.current-date').textContent = `${data.current.last_updated}`;
         document.querySelector('.location').textContent = `${data.location.name}`;
-        /* document.querySelector('.max-temperature').textContent = `${data.forecast.forecastday[2].maxtemp_C}°C`; */
+        /* document.querySelector('.max-temperature').textContent = `${data.forecast.forecastday[0].day.maxtemp_c}°C`; */
         document.querySelector('.current-temperature').textContent = `${Math.ceil(data.current.temp_c)}°C`;
-        document.querySelector('.weather-icon').src = `${data.current.isDay ? 'https://cdn.weatherapi.com/weather/64x64/day/' : 'https://cdn.weatherapi.com/weather/64x64/night/'}${data.current.condition.code}.png`;
+        document.querySelector('.weather-icon').src = `${data.current.condition.icon}`;
         document.querySelector('.precipitation').textContent = `${data.current.precip_mm} mm`;
         document.querySelector('.humidity').textContent = `${data.current.humidity}%`;
         document.querySelector('.uv-index').textContent = `${data.current.uv}`;
         document.querySelector('.wind-speed').textContent = `${data.current.wind_kph} km/h`;
         
+        // Forecast per ogni ora
+        /* document.querySelector('.hour0').textContent = `${data.forecastday[0].hour[0].temp_c}°C`; */
+        // Nome e temperatura massima/minima del giorno successivo
+        const tomorrow = new Date(data.location.localtime);
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        document.querySelector('.dayF1').textContent = getDayName('long', tomorrow);
+        const dayAfterTomorrow = new Date(tomorrow);
+        dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 1);
+        document.querySelector('.dayF2').textContent = getDayName('long', dayAfterTomorrow);
+        const twoDaysAfterTomorrow = new Date(tomorrow);
+        twoDaysAfterTomorrow.setDate(twoDaysAfterTomorrow.getDate() + 2);
+        document.querySelector('.dayF3').textContent = getDayName('long', twoDaysAfterTomorrow);
         /* updateForecastData(); */
       })
 }
@@ -31,6 +47,24 @@ function fetchWeatherData(loc) {
 function updateForecastData(){
     
 }
+
+const tempValues = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
+
+new Chart(document.getElementById("temperatureChart"), {
+    type: 'line',
+    data: {
+        labels: tempValues,
+        datasets: [{
+            label: 'Temperature',
+            data: [10, 10, 17, 21, 25, 27, 25, 15, 20, 25, 27, 35],
+            fill: true,
+            borderColor: 'rgb(255, 217, 0)',
+            tension: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }});
 
 /* Event Listener per trovare le previsioni meteo in base alla posizione inserita */
 document.querySelector(".location-form").addEventListener("submit", (event)=>{
@@ -41,7 +75,6 @@ document.querySelector(".location-form").addEventListener("submit", (event)=>{
   }
 })
 
-
 /* Geolocalizzazione per prendere la posizione dell'utente */
 navigator.geolocation.getCurrentPosition(
   (position) => {
@@ -50,5 +83,5 @@ navigator.geolocation.getCurrentPosition(
     const loc = `${lat},${lon}`;
     fetchWeatherData(loc);
   }, error => {
-    console.error('Location error:', error);
+    console.error('Location not found:', error);
   })
